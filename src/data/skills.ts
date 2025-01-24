18ccf97eddd2b6bcd3281b764cda9150a9e64876
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-01-24 21:33:32 +0700

    adjust Home.tsx, create skill.ts and export it to Home.ts

---
+++ src/data/skills.ts
@@ -0,0 +1,26 @@
+export const skills = [
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+    alt: 'typescript',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    alt: 'javascript',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg',
+    alt: 'tailwindcss',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+    alt: 'css',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+    alt: 'html',
+  },
+  {
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg',
+    alt: 'canva',
+  },
+];
