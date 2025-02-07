467e84ff2ec7f56eb8eb52b098cc1e5659d8f2f6
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-02-07 20:41:46 +0700

    completed Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+import tailwindScrollbar from 'tailwind-scrollbar';
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindScrollbar],
+};
